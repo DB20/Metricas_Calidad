1f6e8f93cea2ff7d5d96b54e2ac6da6ae1c086b3
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2023-11-04 16:35:39 -0500

    cambio de repo

--- routes/tests/mocks.py
+++
@@ -1,9 +0,0 @@
-from httmock import HTTMock, all_requests
-
-@all_requests
-def mock_success_auth(url, request):
-  return { 'status_code': 200 }
-
-@all_requests
-def mock_failed_auth(url, request):
-  return { 'status_code': 401 }
